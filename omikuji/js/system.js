82f3ad10305a101656ade7a8ff2552adaa8302f1
mashu-png <85191016+mashu-png@users.noreply.github.com>
2021-06-02 08:42:41 +0900

    Update system.js

--- omikuji/js/system.js
+++ omikuji/js/system.js
@@ -14,13 +14,14 @@ const doc = document.getElementById('Res');
 const doc1 = document.getElementById('Res1');
 const doc2 = document.getElementById('Items_Title');
 const item_HTML = document.getElementById('Item');
+const Items_Text = "の方のラッキーアイテムはこちら↓";
 const Res_Main = ["今日一日は<br>いい日になるでしょう","今日一日は<br>多少いい事が<br>あるでしょう","今日一日は<br>平凡な日に<br>なるでしょう","今日はいつもより<br>ついてない日です","今日は最悪な日に<br>なるでしょう"];
 const Hcode = ["<h3>","</h3>","<br>","<p>","</p>","<span>","</span>","<h5>","</h5>","<h6>","</h3>"];
 function Systems(){
   if(Random(4) == 0){
     doc.innerHTML = Hcode[5] + Res_title[0] + Hcode[6];
     doc1.innerHTML = Hcode[3] + Res_Main[0] + Hcode[4];
-    doc2.innerHTML = Hcode[7] + Res_title[0] + Hcode[8] + "の方のラッキーアイテムはこちら↓";
+    doc2.innerHTML = Hcode[7] + Res_title[0] + Hcode[8] + Items_Text;
     if(luckyItems(5) == 0){
         item_HTML.innerHTML = Hcode[9] + Item[0] + Hcode[10];
     }else if(luckyItems(5) == 1){
@@ -37,7 +38,7 @@ function Systems(){
   }else if(Random(4) == 1){
     doc.innerHTML = Hcode[5] + Res_title[1] + Hcode[6];
     doc1.innerHTML = Hcode[3] + Res_Main[1] + Hcode[4];
-    doc2.innerHTML = Hcode[7] + Res_title[1] + Hcode[8] + "の方のラッキーアイテムはこちら↓";
+    doc2.innerHTML = Hcode[7] + Res_title[1] + Hcode[8] + Items_Text;
     if(luckyItems(5) == 0){
         item_HTML.innerHTML = Hcode[9] + Item[0] + Hcode[10];
     }else if(luckyItems(5) == 1){
@@ -54,7 +55,7 @@ function Systems(){
   }else if(Random(4) == 2){
     doc.innerHTML = Hcode[5] + Res_title[2] + Hcode[6];
     doc1.innerHTML = Hcode[3] + Res_Main[2] + Hcode[4];
-    doc2.innerHTML = Hcode[7] + Res_title[2] + Hcode[8] + "の方のラッキーアイテムはこちら↓";
+    doc2.innerHTML = Hcode[7] + Res_title[2] + Hcode[8] + Items_Text;
     if(luckyItems(5) == 0){
         item_HTML.innerHTML = Hcode[9] + Item[0] + Hcode[10];
     }else if(luckyItems(5) == 1){
@@ -71,7 +72,7 @@ function Systems(){
   }else if(Random(4) == 3){
     doc.innerHTML = Hcode[5] + Res_title[3] + Hcode[6];
     doc1.innerHTML = Hcode[3] + Res_Main[3] + Hcode[4];
-    doc2.innerHTML = Hcode[7] + Res_title[3] + Hcode[8] + "の方のラッキーアイテムはこちら↓";
+    doc2.innerHTML = Hcode[7] + Res_title[3] + Hcode[8] + Items_Text;
     if(luckyItems(5) == 0){
         item_HTML.innerHTML = Hcode[9] + Item[0] + Hcode[10];
     }else if(luckyItems(5) == 1){
@@ -88,7 +89,7 @@ function Systems(){
   }else{
     doc.innerHTML =　Hcode[5] + Res_title[4] + Hcode[6];
     doc1.innerHTML = Hcode[3] + Res_Main[4] + Hcode[4];
-    doc2.innerHTML = Hcode[7] + Res_title[4] + Hcode[8] + "の方のラッキーアイテムはこちら↓";
+    doc2.innerHTML = Hcode[7] + Res_title[4] + Hcode[8] + Items_Text;
     if(luckyItems(5) == 0){
         item_HTML.innerHTML = Hcode[9] + Item[0] + Hcode[10];
     }else if(luckyItems(5) == 1){
